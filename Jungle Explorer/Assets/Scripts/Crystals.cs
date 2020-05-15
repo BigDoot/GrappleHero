88a396ab85519e3c9d9f5a725f1cb30134fad360
BigDoot <glendonchua@gmail.com>
2020-05-15 15:20:50 +0800

    Added Project

---
+++ Jungle Explorer/Assets/Scripts/Crystals.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystals : MonoBehaviour
+{
+
+    private PlayerMovement player;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+    }
+
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+            player.crystals++;
+        }
+    }
+}
