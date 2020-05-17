1425f3c4a56484b4ef434166c3af8cc6b593ce53
BigDoot <glendonchua@gmail.com>
2020-05-17 13:57:43 +0800

    Update project name

---
+++ GrappleHero/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    public bool jump = false;
+    public bool crouch = false;
+    public Animator animator;
+    public int crystals;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+        
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
---
+++ GrappleHero/Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    Text text;
+    private PlayerMovement player;
+
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+        player = FindObjectOfType<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        text.text = "Crystals: " + player.crystals;
+    }
+}
