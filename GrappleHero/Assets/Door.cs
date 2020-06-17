06307228f61ad62d9c1dcc0e5d545ad500f2752c
BigDoot <glendonchua@gmail.com>
2020-06-17 18:07:18 +0800

    Add DoorToNextLevel prefab
    -Doors in tutorial and level 1 now transition to next level

---
+++ GrappleHero/Assets/Door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public LevelLoader lvlloader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lvlloader = FindObjectOfType<LevelLoader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        lvlloader.LoadNextLevel();
+    }
+}
